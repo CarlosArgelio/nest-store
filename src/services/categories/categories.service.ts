1bc202c5ab85f5ddf367217042fcd8a0debf1e54
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-11-14 13:57:07 -0400

    feat: add services to categories

---
+++ src/services/categories/categories.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import {
+  CreateCategoryDto,
+  UpdateCategoryDto,
+} from 'src/dtos/categories/categories.dtos';
+import { Category } from 'src/entities/categories.entity';
+
+@Injectable()
+export class CategoriesService {
+  private counterId = 1;
+  private categories: Category[] = [
+    {
+      id: 1,
+      name: 'Category 1',
+    },
+  ];
+
+  findAll() {
+    return this.categories;
+  }
+
+  findOne(id: number) {
+    const category = this.categories.find((category) => category.id === id);
+    return category;
+  }
+
+  create(payload: CreateCategoryDto) {
+    this.counterId = this.counterId + 1;
+    const newCategory = {
+      id: this.counterId,
+      ...payload,
+    };
+    this.categories.push(newCategory);
+    return newCategory;
+  }
+
+  update(id: number, payload: UpdateCategoryDto) {
+    const category = this.findOne(id);
+    const index = this.categories.indexOf(category);
+    this.categories[index] = {
+      ...category,
+      ...payload,
+    };
+    return this.categories[index];
+  }
+
+  delete(id: number) {
+    const index = this.categories.indexOf(this.findOne(id));
+    this.categories.splice(index, 1);
+    return true;
+  }
+}
