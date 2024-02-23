7a9f687df21cd1462feff042e9ded050282b22ce
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-23 17:05:11 -0400

    feat: add model & dtos customer

--- src/controllers/costumers/customers.dtos.ts
+++ src/controllers/costumers/customers.dtos.ts
@@ -1,6 +1,6 @@
 import { User } from '../users/users.model';
 import { Customer } from './customers.model';
 
-export interface CreateCustomer extends Customer {
+export interface CreateCustomer extends Omit<Customer, 'customerId'> {
   user: Pick<User, 'email' | 'password'>;
 }
--- src/controllers/costumers/customers.model.ts
+++ src/controllers/costumers/customers.model.ts
@@ -1,6 +1,7 @@
 import { BaseModel } from 'src/base.model';
 
 export interface Customer extends BaseModel {
+  customerId: string;
   name: string;
   lastName: string;
   phone: string;
