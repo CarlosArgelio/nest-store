4272671e834511df4c33b91bfb0c60483ba44f92
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-23 22:01:16 -0400

    feat: add all services to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,11 @@ import { CostumersController } from './controllers/costumers/costumers.controlle
 import { OrdersController } from './controllers/orders/orders.controller';
 import { ProductsController } from './controllers/products/products.controller';
 import { ProductsService } from './services/products/products.service';
+import { UsersService } from './services/users/users.service';
+import { OrdersService } from './services/orders/orders.service';
+import { CostumersService } from './services/costumers/costumers.service';
+import { CategoriesService } from './services/categories/categories.service';
+import { BrandsService } from './services/brands/brands.service';
 
 @Module({
   imports: [],
@@ -20,6 +25,14 @@ import { ProductsService } from './services/products/products.service';
     CostumersController,
     OrdersController,
   ],
-  providers: [AppService, ProductsService],
+  providers: [
+    AppService,
+    ProductsService,
+    UsersService,
+    OrdersService,
+    CostumersService,
+    CategoriesService,
+    BrandsService,
+  ],
 })
 export class AppModule {}
