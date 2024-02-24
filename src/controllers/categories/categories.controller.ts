ada05f4c772b265443061fde12b6614e21060da4
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-23 20:31:36 -0400

    feat: typing caregories

--- src/controllers/categories/categories.controller.ts
+++ src/controllers/categories/categories.controller.ts
@@ -1,18 +1,84 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateCategory } from './categories.dtos';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateCategory, ICategory } from './categories.dtos';
+import { ResponseModel } from 'src/base.model';
+
+const responseFake: ICategory[] = [
+  {
+    categoryId: '1',
+    name: 'Category 1',
+    image: ['image1', 'image2'],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 @Controller('categories')
 export class CategoriesController {
-  @Get('/:categoryId/:productId')
-  getCategory(
-    @Param('categoryId') categoryId: any,
-    @Param('productId') productId: any,
-  ) {
-    return `Product ${productId} and category ${categoryId}`;
+  @Get()
+  findAll(): ResponseModel<ICategory[]> {
+    return {
+      statusCode: 200,
+      data: responseFake,
+    };
+  }
+
+  @Get('/:categoryId')
+  findOne(@Param('categoryId') categoryId: any): ResponseModel<ICategory> {
+    const response = {
+      ...responseFake[0],
+      categoryId,
+    };
+
+    return {
+      statusCode: 200,
+      data: response,
+    };
   }
 
   @Post()
-  create(@Body() payload: CreateCategory) {
-    return payload;
+  create(@Body() payload: CreateCategory): ResponseModel<ICategory> {
+    const response = {
+      ...responseFake[0],
+      ...payload,
+    };
+
+    return {
+      statusCode: 201,
+      data: response,
+    };
+  }
+
+  @Put('/:categoryId')
+  update(
+    @Param('categoryId') categoryId: any,
+    @Body() payload: any,
+  ): ResponseModel<ICategory> {
+    const response = {
+      ...responseFake[0],
+      ...payload,
+      categoryId,
+    };
+
+    return {
+      statusCode: 200,
+      data: response,
+    };
+  }
+
+  @Delete('/:categoryId')
+  delete(@Param('categoryId') categoryId: any): ResponseModel<ICategory> {
+    console.log(categoryId);
+    return {
+      statusCode: 204,
+      data: 'remove',
+    };
   }
 }
--- src/controllers/categories/categories.dtos.ts
+++ src/controllers/categories/categories.dtos.ts
@@ -1,3 +1,4 @@
 import { Category } from './categories.model';
 
+export interface ICategory extends Category {}
 export interface CreateCategory extends Omit<Category, 'categoryId'> {}
