720e2919378b0f2cb56d5c04da3b483525d528b6
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-24 10:21:46 -0400

    feat: add services users

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,4 +1,59 @@
+import { faker } from '@faker-js/faker';
 import { Injectable } from '@nestjs/common';
+import { CreateUser, IUsers, UpdateUser } from 'src/entities/users/users.dtos';
+import { ROLE } from 'src/entities/users/users.model';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: IUsers[] = [];
+
+  constructor() {
+    for (let i = 0; i < 5; i++) {
+      this.users.push({
+        userId: faker.datatype.uuid(),
+        email: faker.internet.email(),
+        role: ROLE.CUSTOMER,
+        password: faker.internet.password(),
+        createdAt: faker.date.past(),
+        updatedAt: faker.date.recent(),
+      });
+    }
+  }
+
+  create(user: CreateUser): IUsers {
+    const userId = uuidv4();
+    const newUser = { ...user, userId };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll(): IUsers[] {
+    return this.users;
+  }
+
+  findOne(id: IUsers['userId']): IUsers {
+    const index = this.users.findIndex((user) => user.userId === id);
+    return this.users[index];
+  }
+
+  findOneByEmail(email: IUsers['email']) {
+    const index = this.users.findIndex((user) => user.email === email);
+    return this.users[index];
+  }
+
+  update(id: IUsers['userId'], changes: UpdateUser) {
+    const index = this.users.findIndex((user) => user.userId === id);
+    this.users[index] = {
+      ...this.users[index],
+      ...changes,
+    };
+  }
+
+  delete(id: IUsers['userId']) {
+    // const users = this.findOne(id);
+    const index = this.users.findIndex((u) => u.userId === id);
+    delete this.users[index];
+    return id;
+  }
+}
