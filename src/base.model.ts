6b747a7f85a4601a351d9628072ca024d7151e2d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-25 19:40:59 -0400

    feat: add baseModel with class

--- src/base.model.ts
+++ src/base.model.ts
@@ -8,3 +8,9 @@ export interface ResponseModel<T> {
   statusCode: number;
   data: T;
 }
+
+export class BaseClassModel {
+  readonly createdAt: Date;
+  readonly updatedAt?: Date;
+  readonly deletedAt?: Date;
+}
