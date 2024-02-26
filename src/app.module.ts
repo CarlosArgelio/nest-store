c138fe933da1953c8786e431c0007fd686a2ebef
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-26 14:53:38 -0400

    feat: implement inject useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './products/controllers/products/products.controller';
-import { ProductsService } from './products/services/products.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+export enum env {
+  development = 'development',
+  production = 'production',
+}
+
+const API_KEY = '123456';
+const API_KEY_PROD = 'PROD_XYZ';
+
 @Module({
   imports: [UsersModule, ProductsModule],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue:
+        process.env.NODE_ENV === env.production ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
