5205e790b8afdf1fb17a86b9ff852a8c48056c13
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-26 17:35:14 -0400

    feat: config documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +13,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true, // Return error if passed params no valid on dtos
     }),
   );
+
+  // Docs
+  const config = new DocumentBuilder()
+    .setTitle('API Nest Store By Carlos Argelio')
+    .setDescription('The api Store')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
