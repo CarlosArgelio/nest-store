f0ebc0d73f05a1721f2aa6c71e2c8f29f53e7e09
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-27 09:35:25 -0400

    test: injection with db

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
+
 import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private readonly apiKey: string,
+    @Inject('PG') private readonly pg: Client,
     @Inject('TASKS') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
   ) {}
@@ -14,5 +17,14 @@ export class AppService {
     console.log(apiKey);
 
     return `Hello World! ${apiKey}`;
+  }
+
+  getHous() {
+    return new Promise((resolve, reject) => {
+      this.pg.query('SELECT NOW()', (err, res) => {
+        if (err) reject(err);
+        resolve(res.rows);
+      });
+    });
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,19 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
+
+const client = new Client({
+  user: 'admin',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect();
+// client.query('SELECT NOW()', (err, res) => {
+//   console.error('🚀 ~ client.query ~ err:', err);
+//   console.log('🚀 ~ client.query ~ res rows:', res.rows);
+// });
 
 export enum env {
   development = 'development',
@@ -17,7 +32,11 @@ const API_KEY_PROD = 'PROD_XYZ';
       useValue:
         process.env.NODE_ENV === env.production ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
