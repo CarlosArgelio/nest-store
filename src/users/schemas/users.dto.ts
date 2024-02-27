38e6d235c777f811f12e314d165b33089a2710c6
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-02-26 20:50:40 -0400

    docs: add docs user dto

--- src/users/schemas/users.dto.ts
+++ src/users/schemas/users.dto.ts
@@ -18,15 +18,26 @@ export class SignUpUserDto extends OmitType(User, [
   })
   readonly email: string;
 
+  @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: 'User password',
+    example: '/234Alfon#_',
+  })
   readonly password: string;
 
+  @IsNotEmpty()
   @IsEnum(ROLE)
+  @ApiProperty({
+    description: 'User role',
+    example: 'costumer',
+    enum: ROLE,
+  })
   readonly role: ROLE;
 }
 
 export class UpdateUserDto extends PartialType(
-  OmitType(SignUpUserDto, ['password']),
+  OmitType(SignUpUserDto, ['password', 'role']),
 ) {}
 
 // export class UpdatePasswordDto extends PickType(User, ['password']) {
