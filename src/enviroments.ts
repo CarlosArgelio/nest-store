856ccff3c1c23af244aeb96ef2282bbeba2f5c93
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-03-05 20:40:02 -0400

    test

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -3,12 +3,10 @@ export enum Environment {
   Production = 'production',
   Staging = 'staging',
 }
-
 export interface EnvironmentConfig {
   name: string;
   file: string;
 }
-
 export const enviroments: Record<Environment, EnvironmentConfig> = {
   [Environment.Development]: {
     name: 'development',
