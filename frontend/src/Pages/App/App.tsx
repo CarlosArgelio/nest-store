d8c01d9925df0372ecdb907faf392b70038dfc0d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-19 00:05:23 -0400

    feat: add structure component folder Page

--- frontend/src/Pages/App/App.tsx
+++ frontend/src/Pages/App/App.tsx
@@ -1,7 +1,21 @@
+import { Home } from './../Home';
+import { MyAccount } from './../MyAccount';
+import { MyOrder } from './../MyOrder';
+import { MyOrders } from './../MyOrders';
+import { NotFound } from './../NotFound';
+import { SignIn } from './../SignIn';
+
 function App() {
   return (
     <>
-      <div className="bg-red-100">Hello world!</div>
+      <div className="bg-red-100">
+        <Home />
+        <MyAccount />
+        <MyOrder />
+        <MyOrders />
+        <NotFound />
+        <SignIn />
+      </div>
     </>
   );
 }
---
+++ frontend/src/Pages/MyOrder/MyOrder.tsx
@@ -0,0 +1,8 @@
+function MyOrder() {
+  return (
+    <div>
+      <h1>My Order</h1>
+    </div>
+  );
+}
+export { MyOrder };
