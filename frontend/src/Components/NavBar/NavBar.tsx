f463dddeee0c1ff28c1b6b0a904de2f43a0ee83c
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-19 12:36:38 -0400

    style: add style to component NavBar

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -2,19 +2,17 @@ import { NavLink } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        carlosargelio0104@gmail.com
-        <li>
-          <NavLink to={'/my-orders'}>My Orders</NavLink>
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+      <ul className="flex items-center gap-3">
+        <li className="font-semibold text-lg">
+          <NavLink to={'/shopi'}>Ecommerce</NavLink>
         </li>
         <li>
-          <NavLink to={'/my-account'}>My Account</NavLink>
+          <NavLink to={'/clothes'}>Clothes</NavLink>
         </li>
         <li>
-          <NavLink to={'/sign-in'}>Sign In</NavLink>
+          <NavLink to={'/electronics'}>Electronics</NavLink>
         </li>
-        <li>CARRITO 0</li>
         <li>
           <NavLink to={'/furnitures'}>Furnitures</NavLink>
         </li>
@@ -24,6 +22,19 @@ export const NavBar = () => {
         <li>
           <NavLink to={'/others'}>Others</NavLink>
         </li>
+      </ul>
+      <ul className="flex items-center gap-3">
+        <li>carlosargelio0104@gmail.com</li>
+        <li>
+          <NavLink to={'/my-orders'}>My Orders</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/my-account'}>My Account</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/sign-in'}>Sign In</NavLink>
+        </li>
+        <li>🛒 0</li>
       </ul>
     </nav>
   );
