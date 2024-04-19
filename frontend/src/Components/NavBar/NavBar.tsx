10378132135e95a769e466421d62adcb99c6292b
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-19 13:14:20 -0400

    refactor: map component navbar

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -1,47 +1,91 @@
 import { NavItem } from './../NavItem';
 
 export const NavBar = () => {
-  const activeStyle = 'underline';
-
   return (
     <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
       <ul className="flex items-center gap-3">
-        <li className="font-semibold text-lg">
-          <NavItem to="/shopi" style={activeStyle} children="Ecommerce" />
-        </li>
-        <li>
-          <NavItem to="/clothes" style={activeStyle} children="Clothes" />
-        </li>
-        <li>
-          <NavItem
-            to="/electronics"
-            style={activeStyle}
-            children="Electronics"
-          />
-        </li>
-        <li>
-          <NavItem to="/furnitures" style={activeStyle} children="Furnitures" />
-        </li>
-        <li>
-          <NavItem to="/toys" style={activeStyle} children="Toys" />
-        </li>
-        <li>
-          <NavItem to="/others" style={activeStyle} children="Others" />
-        </li>
+        {NavItems[0].map((item) => (
+          <li className={item.styleLi ? item.styleLi : ''}>
+            <NavItem to={item.to} style={item.styleItem} children={item.text} />
+          </li>
+        ))}
       </ul>
       <ul className="flex items-center gap-3">
-        <li>carlosargelio0104@gmail.com</li>
-        <li>
-          <NavItem to="/my-orders" style={activeStyle} children="My Orders" />
-        </li>
-        <li>
-          <NavItem to="/my-account" style={activeStyle} children="My Account" />
-        </li>
-        <li>
-          <NavItem to="/sign-in" style={activeStyle} children="Sign In" />
-        </li>
+        <li className="text-black/60">carlosargelio0104@gmail.com</li>
+        {NavItems[1].map((item) => (
+          <li className={item.styleLi ? item.styleLi : ''}>
+            <NavItem to={item.to} style={item.styleItem} children={item.text} />
+          </li>
+        ))}
         <li>🛒 0</li>
       </ul>
     </nav>
   );
 };
+
+const NavItems = [
+  [
+    {
+      styleLi: 'font-semibold text-lg',
+      to: '/shopi',
+      styleItem: 'underline underline-offset-4',
+      text: 'Ecommerce',
+    },
+    {
+      styleLi: '',
+      to: '/',
+      styleItem: 'underline underline-offset-4',
+      text: 'All',
+    },
+    {
+      styleLi: '',
+      to: '/clothes',
+      styleItem: 'underline underline-offset-4',
+      text: 'Clothes',
+    },
+    {
+      styleLi: '',
+      to: '/electronics',
+      styleItem: 'underline underline-offset-4',
+      text: 'Electronics',
+    },
+    {
+      styleLi: '',
+      to: '/furnitures',
+      styleItem: 'underline underline-offset-4',
+      text: 'Furnitures',
+    },
+    {
+      styleLi: '',
+      to: '/toys',
+      styleItem: 'underline underline-offset-4',
+      text: 'Toys',
+    },
+    {
+      styleLi: '',
+      to: '/others',
+      styleItem: 'underline underline-offset-4',
+      text: 'Others',
+    },
+  ],
+  [
+    {
+      styleLi: '',
+      to: '/my-orders',
+      styleItem: 'underline underline-offset-4',
+      text: 'My Orders',
+    },
+    {
+      styleLi: '',
+      to: '/my-account',
+      styleItem: 'underline underline-offset-4',
+      text: 'My Account',
+    },
+    {
+      styleLi: '',
+      to: '/sign-in',
+      styleItem: 'underline underline-offset-4',
+      text: 'Sign In',
+    },
+  ],
+];
