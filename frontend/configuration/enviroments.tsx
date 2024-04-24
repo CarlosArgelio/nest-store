7fb54bdeb75a8e667ae795c9df3a2ab7288aa02d
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-23 20:33:16 -0400

    feat: get api

---
+++ frontend/configuration/enviroments.tsx
@@ -0,0 +1,5 @@
+const API_BASE = import.meta.env.VITE_BASE_API;
+
+export const config = {
+  apiUrl: API_BASE,
+};
--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,10 +1,47 @@
+import { useEffect, useState } from 'react';
 import { Layout, Card } from './../../Components';
 
+import { config } from './../../../configuration';
+
+const { apiUrl } = config;
+
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export interface ProductsGet {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  category: Category;
+  images: string[];
+}
+
 function Home() {
+  const [products, setProducts] = useState<ProductsGet[] | null>(null);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/api/v1/products`)
+      .then((response) => response.json())
+      .then((products) => setProducts(products))
+      .catch((error) => console.log(error))
+      .finally(() => console.log('finally'));
+  }, []);
+
+  if (!products) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Layout>
         Home
+        {products.map(() => (
+          <Card />
+        ))}
         <Card />
       </Layout>
     </>
