e15932a12ec204a281ef789e30026abfd806790b
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-24 17:56:11 -0400

    feat: add styles css to tailwind

--- frontend/src/Components/ProductDetail/ProductDetail.tsx
+++ frontend/src/Components/ProductDetail/ProductDetail.tsx
@@ -1,8 +1,6 @@
-import './styles.css';
-
 export const ProductDetail = () => {
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg">
+    <aside className="product-detail flex flex-col fixed right-0 border border-black bg-white rounded-lg h-[calc(100vh-80px)] w-[360px] top-[68px]">
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>X</div>
